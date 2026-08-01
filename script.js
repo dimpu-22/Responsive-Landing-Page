8877d02830e24b41e6fc99e2f307ed467de60ee1
dimpu_22 <jaswanthj901@gmail.com>
2026-08-01 22:11:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
